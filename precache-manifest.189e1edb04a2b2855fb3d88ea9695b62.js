631da1ab5b4fde02c3c66c3bc8ab4c069888e3a4
Mario Lema <lema_mario@yahoo.com>
2019-04-16 16:58:14 +0100

    Updates

---
+++ precache-manifest.189e1edb04a2b2855fb3d88ea9695b62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "392bad9b469291f0c2e3",
+    "url": "/react-countries/static/js/runtime~main.9462a02b.js"
+  },
+  {
+    "revision": "c02576a6f7b88994a2c2",
+    "url": "/react-countries/static/js/main.09c1035f.chunk.js"
+  },
+  {
+    "revision": "5d4dece2fab8c959ba75",
+    "url": "/react-countries/static/js/2.c3e4aaf9.chunk.js"
+  },
+  {
+    "revision": "c02576a6f7b88994a2c2",
+    "url": "/react-countries/static/css/main.702e366f.chunk.css"
+  },
+  {
+    "revision": "554b6d84c159049428bea6e1c5406d23",
+    "url": "/react-countries/index.html"
+  }
+];
